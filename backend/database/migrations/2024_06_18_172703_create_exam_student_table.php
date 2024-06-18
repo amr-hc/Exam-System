4f42890dbae7ee5c3649c61fa243164468df495f
amr-hc <amro7275@gmail.com>
2024-06-18 21:33:23 +0300

    tables database

---
+++ backend/database/migrations/2024_06_18_172703_create_exam_student_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exam_student', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('exam_id')->constrained('exams')->onDelete('cascade');
+            $table->foreignId('student_id')->constrained('users')->onDelete('cascade');
+            $table->unsignedSmallInteger('score')->default(0);
+            $table->timestamp('started_at')->nullable();
+            $table->enum('status', ['pending', 'ongoing', 'completed', 'expired'])->default('pending');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exam_student');
+    }
+};
