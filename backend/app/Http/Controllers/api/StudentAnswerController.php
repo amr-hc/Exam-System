b9c0cfe549d53fe2dc0a302f5f25a7a53c49d3eb
dabour1 <Aldabor63@Yahoo.Com>
2024-06-20 04:20:41 +0300

    edit assignAnswer &unassignAnswer to accept current authenticated user

--- backend/app/Http/Controllers/api/StudentAnswerController.php
+++ backend/app/Http/Controllers/api/StudentAnswerController.php
@@ -3,23 +3,25 @@
 namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AssignAllAnswersRequest;
-use App\Models\student_answer;
-use App\Models\User;
-use App\Models\Answer;
+// use App\Models\student_answer;
+// use App\Models\User;
+// use App\Models\Answer;
 use Illuminate\Http\Request;
 
 class StudentAnswerController extends Controller
 {
     
-    public function assignAnswer(Request $request,Answer $answer ,User $student){
-        $student->answers()->attach($answer->id);
+    public function assignAnswer(Request $request){
+        auth()->user()->answers()->attach($request->answer_id);
+        // $student->answers()->attach($answer->id);
         return response()->json([
             'message' => 'Answer assigned successfully'
         ], 200);
     }
 
-    public function unassignAnswer(Request $request,Answer $answer ,User $student){
-        $student->answers()->detach($answer->id);
+    public function unassignAnswer(Request $request){
+        auth()->user()->answers()->detach($request->answer_id);
+        // $student->answers()->detach($answer->id);
         return response()->json([
             'message' => 'Answer detached successfully'
         ], 200);
